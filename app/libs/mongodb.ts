312334d4cae76cfabbc65d6e933453646aa33ac0
leonard adjei <157623061+leonardeganow@users.noreply.github.com>
2024-07-04 21:31:38 +0000

    added types ot mongodb connection

--- app/libs/mongodb.ts
+++ app/libs/mongodb.ts
@@ -1,30 +1,45 @@
-import mongoose from 'mongoose';
+import mongoose, { ConnectOptions } from "mongoose";
 
-const MONGODB_URI : string = process.env.MONGODB_URI;
+const MONGODB_URI: string | undefined = process.env.MONGODB_URI;
 
 if (!MONGODB_URI) {
-  throw new Error('Please define the MONGODB_URI environment variable inside .env.local');
+  throw new Error(
+    "Please define the MONGODB_URI environment variable inside .env.local"
+  );
 }
 
-let cached = global.mongoose ;
+interface MongooseCache {
+  conn: typeof mongoose | null;
+  promise: Promise<typeof mongoose> | null;
+}
+
+// Ensure the `global` object is correctly typed
+declare global {
+  var mongoose: MongooseCache;
+}
+
+let cached = global.mongoose;
 
 if (!cached) {
   cached = global.mongoose = { conn: null, promise: null };
 }
 
-async function connectToDatabase() {
+async function connectToDatabase(): Promise<typeof mongoose> {
   if (cached.conn) {
     return cached.conn;
   }
 
   if (!cached.promise) {
-    cached.promise = mongoose.connect(MONGODB_URI, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    }).then((mongoose) => {
-      return mongoose;
-    });
+    cached.promise = mongoose
+      .connect(MONGODB_URI as string, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+      } as ConnectOptions)
+      .then((mongoose) => {
+        return mongoose;
+      });
   }
+
   cached.conn = await cached.promise;
   return cached.conn;
 }
