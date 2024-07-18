e58367ebfe82ec5248ecbb956a6f344e171c6df8
leonard adjei <157623061+leonardeganow@users.noreply.github.com>
2024-07-18 16:28:09 +0000

    added mobile menu

--- components/header/MobileMenu.tsx
+++ components/header/MobileMenu.tsx
@@ -1,34 +1,81 @@
 "use client";
-import React from "react";
+import React, { useState } from "react";
 import { navItems } from "../dashboard/Sidebar";
 import Logo from "./Logo";
-import { useRouter } from "next/navigation";
+import { usePathname, useRouter } from "next/navigation";
 
 interface MobileMenuProps {
-  user: CurrentUser;
+  user: {
+    role: string;
+  };
 }
-function MobileMenu(props: MobileMenuProps) {
+
+function MobileMenu({ user }: MobileMenuProps) {
   const router = useRouter();
+  const pathname = usePathname();
+  const [openSubMenus, setOpenSubMenus] = useState<{ [key: string]: boolean }>(
+    {}
+  );
 
   // Filter navItems based on user role
   const filteredNavItems = navItems.filter((item) =>
-    item.roles.includes(props.user.role)
+    item.roles.includes(user.role)
   );
+
+  const handleSubMenuToggle = (label: string) => {
+    setOpenSubMenus((prev) => ({
+      ...prev,
+      [label]: !prev[label],
+    }));
+  };
 
   const renderNavItems = () => {
     return filteredNavItems.map((item) => (
-      <div
-        onClick={() => router.push(item.path)}
-        key={item.label}
-        className={`hover:bg-red-100 hover:text-red-800 dark:hover:bg-white dark:hover:text-black   p-4 mb-2 rounded-lg cursor-pointer transition-all duration-100 `}
-      >
-        <div className={`flex items-center  font-medium gap-x-4`}>
-          {item.icon}
-          <p>{item.label}</p>{" "}
+      <div key={item.label}>
+        <div
+          onClick={() =>
+            item.subMenu
+              ? handleSubMenuToggle(item.label)
+              : router.push(item.path)
+          }
+          className={`hover:bg-red-100 hover:text-red-800 dark:hover:bg-white dark:hover:text-black p-4 mb-2 rounded-lg cursor-pointer transition-all duration-100 ${
+            item.path === pathname
+              ? "border-r-2 border-red-600 dark:border-white bg-red-200 text-red-800"
+              : ""
+          }`}
+        >
+          <div className={`flex items-center font-medium gap-x-4`}>
+            {item.icon}
+            <p>{item.label}</p>
+          </div>
         </div>
+        {item.subMenu && (
+          <div
+            className={`pl-8 overflow-hidden transition-all duration-300 ${
+              openSubMenus[item.label] ? "max-h-screen" : "max-h-0"
+            }`}
+          >
+            {item.subMenu.map((subItem) => (
+              <div
+                onClick={() => router.push(subItem.path)}
+                key={subItem.label}
+                className={`hover:bg-red-100 hover:text-red-800 dark:hover:bg-white dark:hover:text-black p-4 mb-2 rounded-lg cursor-pointer transition-all duration-100 ${
+                  subItem.path === pathname
+                    ? "border-r-2 border-red-600 dark:border-white bg-red-200 text-red-800"
+                    : ""
+                }`}
+              >
+                <div className="flex items-center font-medium gap-x-4">
+                  <p>- {subItem.label}</p>
+                </div>
+              </div>
+            ))}
+          </div>
+        )}
       </div>
     ));
   };
+
   return (
     <div>
       <Logo route="/dashboard" />
